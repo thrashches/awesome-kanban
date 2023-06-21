dc264a0069916f5d1962ed960a25ef0047083a17
thrashches <thrashches>
2023-06-21 17:52:03 +0300

    Some

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import style from "./Dropdown.module.scss";
 
 export function Dropdown (props) {
+  const {items} = props;
+
   return <div className={style.Dropdown}>
-    
+    {items?items.map()}
   </div>
 }
