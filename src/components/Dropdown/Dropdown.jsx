41bcfcda816580105b8bbf81e45fa37306a88ec0
thrashches <thrashches>
2023-06-22 18:13:20 +0300

    Dropdown toggle style

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Dropdown.module.scss";
+import { ReactComponent as ArrowIcon } from "./Arrow.svg";
 
-export function Dropdown (props) {
-  const {items} = props;
+export function Dropdown(props) {
+  const { items } = props;
+  const emptySelection = {
+    id: null,
+    title: "",
+  }
+  const [selected, setSelected] = useState(emptySelection);
 
-  return <>
+  return (
+    <>
+      <div className={style.DropdownToggle}>
+        <div>{selected.title}</div>
+        <ArrowIcon/>
+      </div>
 
-  <div className={style.DropdownInput}>
-
-  </div>
-  
-  <div className={style.Dropdown}>
-    {items?items.map(item => <div>{item.title}</div>):<></>}
-  </div>
-  </>
+      <div className={style.Dropdown}>
+        {items ? items.map((item) => <div>{item.title}</div>) : <></>}
+      </div>
+    </>
+  );
 }
